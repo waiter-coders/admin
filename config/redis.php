c02feb617c8c4ea2f5f8184563df854b9521fba5
tian8242382 <949855040@qq.com>
2018-03-01 10:00:54 +0800

    整体结构调整

---
+++ config/redis.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 94985
+ * Date: 2018/2/28
+ * Time: 22:49
+ */
---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 94985
+ * Date: 2018/3/1
+ * Time: 8:56
+ */
