0774ead916259be0b7353cee33e80e62c46410b8
tian.zheng <tian.zheng@zol.com.cn>
2019-02-18 10:19:01 +0800

    移动config

---
+++ src/Config/Action/Row.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/16
+ * Time: 13:59
+ */
+
+namespace Waiterphp\Admin\Config\Action;
+
+
+class Row extends Base
+{
+    public function __construct($action)
+    {
+        parent::__construct($action);
+        $default = self::defaultRowActions();
+        if (isset($default[$action])) {
+            $this->config = array_merge($this->config, $default[$action]);
+        }
+    }
+
+    private static function defaultRowActions()
+    {
+        return [
+            'edit'=>['type'=>'page', 'name'=>'编辑', 'url'=>'editor?@primaryKey@=@data.id@'],
+            'delete'=>[
+                'type'=>'ajax', 'name'=>'删除',
+                'confirm'=>'您确定要删除@data.id@吗？',
+                'url'=>'delete/?@primaryKey@=@data.id@'
+            ],
+            'audit'=>['type'=>'ajax', 'name'=>'审核',  'url'=>'audit'],
+        ];
+    }
+
+}
